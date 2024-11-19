4fb87b725856c67d54b31c8ad633776fc173332f
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2024-11-20 02:15:19 +0700

    renew logo and website name

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <nav className="navbar bg-gradient-to-r from-yellow-500 to-orange-500 mx-auto w-full fixed top-0 left-0 px-2 sm:px-6 lg:px-8">
       <div className="container mx-auto flex justify-between items-center py-4">
         <h1 className="text-2xl font-bold text-white flex gap-1">
-          <img src="/logo/indah_logo.png" alt="Logo" className="w-32 h-4 mt-3 mx-auto" />
+          {/* <img src="/logo/indah_logo.png" alt="Logo" className="w-32 h-4 mt-3 mx-auto" /> */}
           <a href="#home" onClick={() => handleScrollTo("home")}>
             Indah Travel
           </a>
