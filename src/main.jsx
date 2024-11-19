a95c4349e95fe666b17ba2d02d08785cadea6e3d
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2024-11-20 02:40:52 +0700

    add component ArmadaCard with props

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbar from "./component/Navbar.jsx";
 import SelamatDatang from "./component/SelamatDatang.jsx";
+import ArmadaCard from "./component/Armada.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
     <SelamatDatang />
+    <ArmadaCard nama={"Indah Travel"} image={"./logo/indah_logo.png"} deskripsi={"Kami hadir untuk memberikan solusi terbaik bagi kebutuhan Perjalanan Anda."} kapasitas={4} />
   </StrictMode>
 );
